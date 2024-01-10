2c7b97a1b38b73bad014e7f14daffb3fd6b89183
RajbharSaheb <110810360+RajbharSaheb@users.noreply.github.com>
2024-01-10 13:06:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -570,6 +570,10 @@ async def filter_seasons_cb_handler(client: Client, query: CallbackQuery):
     
     if not settings["button"]:
         cur_time = datetime.now(pytz.timezone('Asia/Kolkata')).time()
+
+
+
+        
         time_difference = timedelta(hours=cur_time.hour, minutes=cur_time.minute, seconds=(cur_time.second+(cur_time.microsecond/1000000))) - timedelta(hours=curr_time.hour, minutes=curr_time.minute, seconds=(curr_time.second+(curr_time.microsecond/1000000)))
         remaining_seconds = "{:.2f}".format(time_difference.total_seconds())
         total_results = len(files)
