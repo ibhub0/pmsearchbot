3d9ab2b4df269593566ba0108bcf6cf1de074ebf
RajbharSaheb <110810360+RajbharSaheb@users.noreply.github.com>
2024-03-10 09:16:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,8 +51,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'YourDemandZone')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'sharelinks.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '8709f8b09c3f169265a6935c4587498c745125d8')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'earnpro.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '89abd24b5f7189c009f73d546c74487edeeb5676')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
