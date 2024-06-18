9d7ab9665512979d748f617d16874b30adccb20f
INFINITY BOTZ <132870552+ibhub0@users.noreply.github.com>
2024-06-18 21:54:02 +0530

    Updated

--- info.py
+++ info.py
@@ -94,8 +94,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://rbtg-ddabfd6b090e.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://rbtg-ddabfd6b090e.herokuapp.com/".format(FQDN, PORT)
+URL = "https://pmsearchbot-infinitybotz.onrender.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://pmsearchbot-infinitybotz.onrender.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -110,13 +110,13 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://rbtg-ddabfd6b090e.herokuapp.com/".format(FQDN)
+    URL = "https://pmsearchbot-infinitybotz.onrender.com/".format(FQDN)
 else:
-    URL = "https://rbtg-ddabfd6b090e.herokuapp.com/".format(FQDN)
+    URL = "https://pmsearchbot-infinitybotz.onrender.com/".format(FQDN)
 
 
 
-LANGUAGES = ["malayalam", "mal", "tamil", "tam" ,"english", "eng", "hindi", "hin", "telugu", "tel", "kannada", "kan"]
+LANGUAGES = ["english", "eng", "bengali", "ben", "hindi", "hin", "malayalam", "mal", "tamil", "tam", "telugu", "tel", "kannada", "kan", "marathi", "mar"]
 
 QUALITY = ["PreDvd", "S Print", "HDTS", "HDTC", "480P", "720P" ,"1080P", "2160P"]
 SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "season 6" , "season 7" , "season 8" , "season 9" , "season 10"]
